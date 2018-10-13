bd9d56579a600470df775c6148a9c210f1472175
Pezze18 <dallepezzedavide@gmail.com>
2018-10-13 08:17:11 +0200

    Aggiornato convertTime file

--- src/test/convertTime.py
+++ src/test/convertTime.py
@@ -1,10 +1,24 @@
 import sys
 
-time="745m11.742s"
-(time,seconds)=time.split("m")
-time=int(time)
-seconds=int(float(seconds.split("s")[0]))
+times=["0m11.588s", " 0m15.902s", "4m48.703s", "403m10.665s"]
+times_trad=[]
+for time in times:
+    (time,seconds)=time.split("m")
+    time=int(time)
+    seconds=(float(seconds.split("s")[0]))
 
-h=int(time/60)
-m=time-60*h 
-print(str(h)+":"+str(m)+":"+str(seconds))
+    ms=seconds-int(seconds)
+    seconds=int(seconds)
+
+    h=int(time/60)
+    m=time-60*h
+    if h==0:
+        h="00"
+    if m==0:
+        m="00"
+    if seconds==0:
+        seconds="00"
+    ms=str(ms*1000)[0:3]
+
+    times_trad.append(str(h)+"h:"+str(m)+"m:"+str(seconds)+"s:"+str(ms)+"ms")
+print(times_trad)
